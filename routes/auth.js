bdb4535f96bb6efbf908b3e9dfbbc747a2290924
hunit <hunit612@naver.com>
2023-09-19 00:32:50 +0900

    feat: 카카오 로그인 추가

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,22 @@ const passport = require('passport');
 
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 
-const { join, login, logout } = require('../controllers');
+const { join, login, logout } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/join', isNotLoggedIn, join);
 router.post('/login', isNotLoggedIn, login);
 router.get('/logout', isLoggedIn, logout);
+
+// GET /auth/kakao
+router.get('/kakao', passport.authenticate('kakao'));
+
+// GET /auth/kakao/callback
+router.get('/kakao/callback', passport.authenticate('kakao', {
+  failureRedirect: '/?error=fail kakao login',
+}), (req, res) => {
+  res.redirect('/');
+});
 
 module.exports = router;
