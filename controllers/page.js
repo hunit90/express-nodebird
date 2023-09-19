4ae65b86af7bd03850886bef0d33746b846cbff6
hunit <hunit612@naver.com>
2023-09-20 00:36:05 +0900

    post 업로드 추가 팔로우 팔로잉 기능 추가

--- controllers/page.js
+++ controllers/page.js
@@ -1,3 +1,5 @@
+const { User, Post, Hashtag } = require('../models');
+
 exports.renderProfile = (req, res) => {
   res.render('profile', { title: 'my Information - NodeBird' });
 };
@@ -6,10 +8,43 @@ exports.renderJoin = (req, res) => {
   res.render('join', {title: 'sign up - NodeBird' });
 };
 
-exports.renderMain = (req, res, next) => {
-  const twits = [];
-  res.render('main', {
-    title: 'NodeBird',
-    twits,
-  });
+exports.renderMain = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
+
+exports.renderHashtag = async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashing.getPosts({ include: [{ model: User }] });
+    }
+
+    return res.render('main', {
+      title: `${query} | NodeBird`,
+      twits: posts,
+    });
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+}
--- routes/page.js
+++ routes/page.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { renderProfile, renderJoin, renderMain } = require('../controllers/page');
+const { renderProfile, renderJoin, renderMain, renderHashtag, } = require('../controllers/page');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  res.locals.user = null;
-  res.locals.followerCounter = 0;
-  res.locals.followingCounter = 0;
-  res.locals.followingIdList = [];
+  res.locals.followerCounter = req.user?.Followers?.length || 0;
+  res.locals.followingCounter = req.user?.Followings?.length || 0;
+  res.locals.followingIdList = req.user?.Followings?.map(f=> f.id) || [];
   next();
 });
 
+router.get('/hashtag', renderHashtag);
 router.get('/profile', isLoggedIn, renderProfile);
 router.get('/join', isNotLoggedIn, renderJoin);
 router.get('/', renderMain);
